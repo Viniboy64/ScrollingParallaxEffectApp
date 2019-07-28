a104b5c37ba4be4c728fb2601d6dab0ab513a8d3
Marcus Vinícius <veras.marcusvinicius@gmail.com>
2019-07-28 18:18:36 -0300

    tableview-related-files-added

---
+++ ScrollingParallaxEffectApp/TableView/TableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  TableViewCell.swift
+//  ScrollingParallaxEffectApp
+//
+//  Created by Marcus Vinícius on 28/07/19.
+//  Copyright © 2019 Marcus Vinícius. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code.
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        // Configure the view for the selected state.
+    }
+}
