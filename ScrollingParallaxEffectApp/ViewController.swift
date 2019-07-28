6d77e8734655d019a16a5d40359d278e69e17a64
Marcus Vinícius <veras.marcusvinicius@gmail.com>
2019-07-28 15:44:23 -0300

    Initial Commit

---
+++ ScrollingParallaxEffectApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScrollingParallaxEffectApp
+//
+//  Created by Marcus Vinícius on 28/07/19.
+//  Copyright © 2019 Marcus Vinícius. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
